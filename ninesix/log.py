1689ba30caaefe82ea4d5a9e8ce13a605dd669f3
JogleLew <lvjiagao@outlook.com>
2019-05-13 10:01:04 +0800

    add config hijack

--- ninesix/log.py
+++ ninesix/log.py
@@ -6,6 +6,7 @@
 """ Copyright 2019, Jogle Lew """
 
 import os
+import json
 import inspect
 import logging
 import threading
@@ -36,13 +37,22 @@ class Logger():
         if log_level > self.log_level:
             log_frame = inspect.stack()[1]
             cur_time = util.current_time()
+
+            # config object to json
             if fmt == "json":
                 config_json = cfg
             elif fmt == "argparse":
                 config_json = vars(cfg)
+
+            # config hijack
             if "NINESIX_CONFIG" in os.environ:
-                # TODO Config Hijack
-                pass
+                new_cfg = json.loads(os.environ["NINESIX_CONFIG"])
+                config_json.update(new_cfg)
+                if fmt == "json":
+                    cfg.update(new_cfg)
+                elif fmt == "argparse":
+                    cfg = util.json_to_object(json.dumps(config_json))
+
             for dao in self.daos:
                 dao.msg(config_json, log_frame, cur_time, tag)
         self._lock.release()
