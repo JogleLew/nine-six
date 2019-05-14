a6493792c6679256286d44d1de44ffea538183f9
JogleLew <lvjiagao@outlook.com>
2019-05-14 23:50:19 +0800

    add setup.py and license

---
+++ setup.py
@@ -0,0 +1,33 @@
+import sys
+from setuptools import setup, find_packages
+
+long_description = open('README.md').read()
+
+setup(
+    name='ninesix',
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    version="0.0.1",
+    description='Easy-to-use unified tools for NN logging and managing.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Jogle Lew',
+    author_email='author@example.com',
+    url='https://github.com/JogleLew/nine-six',
+    license='AGPLv3+',
+    python_requires='>=3.0',
+    keywords=['Nine Six', 'nine-six', 'Logging', 'Logger', 'Grid Search'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Utilities"
+    ],
+    entry_points={
+        "console_scripts": [
+            '96grid = ninesix.tool.grid:main'
+        ]
+    }
+)
